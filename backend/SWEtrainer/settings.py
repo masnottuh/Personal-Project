fa5a88f2a29659850d3652eef9d2904bd58c950b
Robert "Sam" Hutton <55932350+masnottuh@users.noreply.github.com>
2023-03-08 17:14:28 -0800

    Update settings.py

--- backend/SWEtrainer/settings.py
+++ backend/SWEtrainer/settings.py
@@ -19,9 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-m*l^%o93jl_i8yu59)fwt81xqbcp&fm_$yeg+5nn--nwr89c2d"
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
